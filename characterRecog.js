bb21f95626cc8701d80b71e56ee6a02572f940b3
Caitlin Croteau <cjcroteau@gmail.com>
2023-06-30 16:39:50 -0700

    add solution to characterRecog.js

---
+++ characterRecog.js
@@ -0,0 +1,40 @@
+//Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+
+// When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+
+// Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+// S is misinterpreted as 5
+// O is misinterpreted as 0
+// I is misinterpreted as 1
+// The test cases contain numbers only by mistake.
+
+function correct(string) {
+  //loop through, find errors, update, return new string.
+  let newString = "";
+
+  for (let i of string) {
+    if (i === "5") {
+      newString += "S";
+    } else if (i === "0") {
+      newString += "O";
+    } else if (i === "1") {
+      newString += "I";
+    } else {
+      newString += i;
+    }
+  }
+  return newString;
+}
+
+console.log(correct("L0ND0N"));
+console.log(correct("DUBL1N"));
+console.log(correct("51NGAP0RE"));
+console.log(correct("BUDAPE5T"));
+console.log(correct("PAR15"));
+
+// "LONDON"
+// "DUBLIN"
+// "SINGAPORE"
+// "BUDAPEST"
+// "PARIS"
