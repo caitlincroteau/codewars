f7e083ddda8fce27e4bc6d10d968563f1f0b87ce
Caitlin Croteau <cjcroteau@gmail.com>
2023-07-05 19:39:45 -0700

    add comments

--- deleteNth.js
+++ deleteNth.js
@@ -7,6 +7,7 @@
 // For example if the input number is 2, and the input list is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3].
 // With list [20,37,20,21] and number 1, the result would be [20,37,21].
 
+//could not solve this! Need to come back to it.
 function deleteNth(arr, n) {
   let count = 1;
 
