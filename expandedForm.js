f7250a205d885a4b03f7fdda260d9e19a82e32d7
Caitlin Croteau <cjcroteau@gmail.com>
2023-07-07 12:50:02 -0700

    clean up

--- expandedForm.js
+++ expandedForm.js
@@ -2,35 +2,35 @@
 // You will be given a number and you will need to return it as a string in Expanded Form. For example:
 // NOTE: All numbers will be whole numbers greater than 0.
 
-// function expandedForm(num) {
-//   const string = String(num);
-//   let resultArr = [];
-//   let count = string.length - 1;
+function expandedForm(num) {
+  const string = String(num);
+  let resultArr = [];
+  let count = string.length - 1;
 
-//   for(let i = 0; i < string.length; i++) {
-//     if(string[i] > 0) {
-//       resultArr.push(string[i] * Math.pow(10, count));
-//     }
-//     count --;
-//   }
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] > 0) {
+      resultArr.push(string[i] * Math.pow(10, count));
+    }
+    count--;
+  }
 
-//   if(resultArr.length > 1){
-//     return resultArr.join(" + ")
-//   }
+  if (resultArr.length > 1) {
+    return resultArr.join(" + ");
+  }
 
-//   return resultArr.toString()
-// }
+  return resultArr.toString();
+}
 
 //took this solution from codewars. Uses more built in methods.
-const expandedForm = (n) =>
-  n
-    .toString()
-    .split("")
-    .reverse()
-    .map((a, i) => a * Math.pow(10, i))
-    .filter((a) => a > 0)
-    .reverse()
-    .join(" + ");
+// const expandedForm = (n) =>
+//   n
+//     .toString()
+//     .split("")
+//     .reverse()
+//     .map((a, i) => a * Math.pow(10, i))
+//     .filter((a) => a > 0)
+//     .reverse()
+//     .join(" + ");
 
 //each step
 // "70304"
@@ -67,3 +67,4 @@ console.log(expandedForm(9000000));
 // Should return '10 + 2'
 // Should return '40 + 2'
 // Should return '70000 + 300 + 4'
+// Should return '9000000'
