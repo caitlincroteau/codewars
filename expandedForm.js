e53be50e6f8d7c0a3570260ea27b776729fd0cb4
Caitlin Croteau <cjcroteau@gmail.com>
2023-07-07 12:49:24 -0700

    add example solution using more methods

--- expandedForm.js
+++ expandedForm.js
@@ -2,28 +2,44 @@
 // You will be given a number and you will need to return it as a string in Expanded Form. For example:
 // NOTE: All numbers will be whole numbers greater than 0.
 
+// function expandedForm(num) {
+//   const string = String(num);
+//   let resultArr = [];
+//   let count = string.length - 1;
 
+//   for(let i = 0; i < string.length; i++) {
+//     if(string[i] > 0) {
+//       resultArr.push(string[i] * Math.pow(10, count));
+//     }
+//     count --;
+//   }
 
-function expandedForm(num) {
-  const string = String(num);
-  let resultArr = [];
-  let count = string.length - 1;
+//   if(resultArr.length > 1){
+//     return resultArr.join(" + ")
+//   }
 
-  for(let i = 0; i < string.length; i++) {
-    if(string[i] > 0) {
-      resultArr.push(string[i] * Math.pow(10, count));
-    }
-    count --;
-  }
+//   return resultArr.toString()
+// }
 
-  if(resultArr.length > 1){
-    return resultArr.join(" + ")
-  }
+//took this solution from codewars. Uses more built in methods.
+const expandedForm = (n) =>
+  n
+    .toString()
+    .split("")
+    .reverse()
+    .map((a, i) => a * Math.pow(10, i))
+    .filter((a) => a > 0)
+    .reverse()
+    .join(" + ");
 
-  return resultArr.toString()
-}
-
-
+//each step
+// "70304"
+// [7, 0, 3, 0, 4]
+// [4, 0, 3, 0, 7]
+// [4, 0, 300, 0, 70000]
+// [4, 300, 70000]
+// [70000, 300, 4]
+// "70000 + 300 + 4"
 
 //this works but it's too complicated. There must be a more mathy way!
 // function expandedForm(num) {
