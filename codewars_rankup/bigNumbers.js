05d294dc835d9340b11304eea41ceec9eb4e7ae0
Caitlin Croteau <cjcroteau@gmail.com>
2023-07-10 15:27:21 -0700

    add solution for bigNumbers.js without using BigInt

--- codewars_rankup/bigNumbers.js
+++ codewars_rankup/bigNumbers.js
@@ -20,25 +20,58 @@
 //   return bigI.toString();
 // }
 
-function add(a, b) {
-  // const array = ["63829983432984289347293874"]
-  // const binaryRep = []
-  let num1 = Number(a).toString(2);
-  let num2 = Number(b).toString(2);
+//version without using BigInt. It works! Woot!!
 
-  let sum = num1 + num2;
-  console.log(num1);
-  console.log(num2);
-  // console.log(array.charCodeAt[0].toString(2))
-  // binaryRep.push(a.toString(2));
-  // return binaryRep
+function add(a, b) {
+  const result = [];
+  let carryOver = 0;
+  const array1 = a.split("");
+  const array2 = b.split("");
+
+  //set arrays if the input strings are of different lengths
+  if (a.length > b.length) {
+    let zeros = a.length - b.length;
+    while (zeros > 0) {
+      array2.splice(0, 0, "0");
+      zeros--;
+    }
+  }
+  if (a.length < b.length) {
+    let zeros = b.length - a.length;
+    while (zeros > 0) {
+      array1.splice(0, 0, "0");
+      zeros--;
+    }
+  }
+
+  //loop through arrays and calculate
+  for (let i = array1.length - 1; i > 0; i--) {
+    //convert elements to numbers and sum
+    const sum = Number(array1[i]) + Number(array2[i]) + carryOver;
+    const sumStr = sum.toString();
+    if (sum > 9) {
+      result.push(sumStr[1]);
+      carryOver = Number(sumStr[0]);
+    } else {
+      result.push(sumStr);
+      carryOver = 0;
+    }
+  }
+  //handle o index + carry over
+  result.push(Number(array1[0]) + Number(array2[0]) + carryOver);
+
+  const reverse = result.reverse();
+  const stringRep = reverse.join("");
+  return stringRep;
 }
 
 console.log(add("20", "30"));
-// console.log(add("123", "321"));
+// "50"
+console.log(add("123", "321"));
 // "444"
-// console.log(add("11", "99"));
+console.log(add("11", "99"));
 // "110"
-
-// console.log(add("63829983432984289347293874", "90938498237058927340892374089"));
+console.log(add("9734", "508"));
+// "10242"
+console.log(add("63829983432984289347293874", "90938498237058927340892374089"));
 // "91002328220491911630239667963"
