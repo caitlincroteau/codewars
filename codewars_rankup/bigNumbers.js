65117ceb7e61a4b29a078df8ad5b7b2a2266363f
Caitlin Croteau <cjcroteau@gmail.com>
2023-07-10 00:21:00 -0700

    add start of solution for bigNumbers.js

--- codewars_rankup/bigNumbers.js
+++ codewars_rankup/bigNumbers.js
@@ -11,15 +11,31 @@
 // The numbers are positives
 
 //this works, but the version of node on this Kyu does not support BigInt
+// function add(a, b) {
+//   const bigI = BigInt(a) + BigInt(b);
+//   return bigI.toString();
+// }
+
 function add(a, b) {
-  const bigI = BigInt(a) + BigInt(b);
-  return bigI.toString();
+  // const array = ["63829983432984289347293874"]
+  // const binaryRep = []
+  let num1 = Number(a).toString(2)
+  let num2 = Number(b).toString(2)
+
+  let sum = num1 + num2
+  console.log(num1)
+  console.log(num2)
+  // console.log(array.charCodeAt[0].toString(2))
+  // binaryRep.push(a.toString(2));
+  // return binaryRep
 }
 
+console.log(add("20", "30"));
 // console.log(add("123", "321"));
-// // "444"
+// "444"
 // console.log(add("11", "99"));
-// // "110"
+// "110"
 
-console.log(add("63829983432984289347293874", "90938498237058927340892374089"));
-//"91002328220491911630239667963"
+
+// console.log(add("63829983432984289347293874", "90938498237058927340892374089"));
+// "91002328220491911630239667963"
