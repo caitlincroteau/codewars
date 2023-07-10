ccbdbdc6296a1cd0797a76b8cb64bea14d1a0bb0
Caitlin Croteau <cjcroteau@gmail.com>
2023-07-10 00:21:45 -0700

    tidy code

--- codewars_rankup/binaryCalculatorTest.js
+++ codewars_rankup/binaryCalculatorTest.js
@@ -5,19 +5,19 @@ function binaryCalculator(num1, num2) {
   const array2 = num2.split("");
 
   //set arrays if the input strings are of different lengths
-  if(num1.length > num2.length) {
+  if (num1.length > num2.length) {
     let zeros = num1.length - num2.length;
     while (zeros > 0) {
-      array2.splice(0, 0, 0)
+      array2.splice(0, 0, 0);
       zeros--;
-    }    
+    }
   }
-  if(num1.length < num2.length) {
+  if (num1.length < num2.length) {
     let zeros = num2.length - num1.length;
     while (zeros > 0) {
-      array1.splice(0, 0, '0')
+      array1.splice(0, 0, "0");
       zeros--;
-    }    
+    }
   }
 
   //loop through arrays and calculate
@@ -37,8 +37,8 @@ function binaryCalculator(num1, num2) {
       carryOver = 1;
     }
   }
-  const binaryResult = result.reverse().join("")
-  const decimalResult = parseInt(binaryResult, 2)
+  const binaryResult = result.reverse().join("");
+  const decimalResult = parseInt(binaryResult, 2);
 
   return decimalResult;
 }
@@ -52,7 +52,6 @@ console.log(binaryCalculator("01100", "0101"));
 // 010001
 // 17
 // 12 + 5
-
 
 console.log(binaryCalculator("0101000001", "01111011"));
 // 0110111100
