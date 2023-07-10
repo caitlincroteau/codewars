d77eeddd73dc9b126ecf7fc83205dabccbbe62e6
Caitlin Croteau <cjcroteau@gmail.com>
2023-07-10 00:26:14 -0700

    add notes to bigNumbers.js

--- codewars_rankup/bigNumbers.js
+++ codewars_rankup/bigNumbers.js
@@ -10,6 +10,10 @@
 // The input is a string of only digits
 // The numbers are positives
 
+//NEED TO FINISH THIS.
+//See this:
+//https://levelup.gitconnected.com/how-to-add-two-very-large-numbers-in-javascript-6305ed8c5fad
+
 //this works, but the version of node on this Kyu does not support BigInt
 // function add(a, b) {
 //   const bigI = BigInt(a) + BigInt(b);
@@ -19,12 +23,12 @@
 function add(a, b) {
   // const array = ["63829983432984289347293874"]
   // const binaryRep = []
-  let num1 = Number(a).toString(2)
-  let num2 = Number(b).toString(2)
+  let num1 = Number(a).toString(2);
+  let num2 = Number(b).toString(2);
 
-  let sum = num1 + num2
-  console.log(num1)
-  console.log(num2)
+  let sum = num1 + num2;
+  console.log(num1);
+  console.log(num2);
   // console.log(array.charCodeAt[0].toString(2))
   // binaryRep.push(a.toString(2));
   // return binaryRep
@@ -35,7 +39,6 @@ console.log(add("20", "30"));
 // "444"
 // console.log(add("11", "99"));
 // "110"
-
 
 // console.log(add("63829983432984289347293874", "90938498237058927340892374089"));
 // "91002328220491911630239667963"
